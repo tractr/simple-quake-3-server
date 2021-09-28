232bd84acd9cde65afe026e6759256a719d21478
Edouard Demotes <e.demotes@gmail.com>
2021-09-28 00:20:34 -0400

    feat: add console service to log connection

---
+++ gui/back/src/api/rcon/console.service.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import { RconService } from './rcon.service';
+
+class ConsoleServiceClass {
+	constructor() {
+		this._rcon = RconService;
+	}
+
+	listen() {
+		this._registerEvents();
+	}
+
+	_registerEvents() {
+		this._rcon.getEmitter().on('newPlayer', (player) => {
+			this._log(
+				`*${player}* has connected`
+			);
+		});
+		this._log(`Server is online!`);
+	}
+
+	/** @param {string} message */
+	_log(message) {
+		console.log(message)
+	}
+}
+
+export const ConsoleService = new ConsoleServiceClass();
